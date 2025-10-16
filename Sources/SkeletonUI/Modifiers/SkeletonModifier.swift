35e2de7600863068782695bda1c43d0ebefd9f2e
Henry Storch <thisisthefoxe@gmail.com>
2025-10-17 08:53:27 +1300

    Add fallback support for iOS <16

--- Sources/SkeletonUI/Modifiers/SkeletonModifier.swift
+++ Sources/SkeletonUI/Modifiers/SkeletonModifier.swift
@@ -13,9 +13,20 @@ public struct SkeletonModifier: ViewModifier {
             .modifier(SkeletonAnimatableModifier(animate ? 1 : 0, appearance))
             .clipShape(SkeletonShape(shape))
             .animation(animation.type, value: animate)
-            .task {
+            .startOnce {
                 guard !animate else { return }
                 animate.toggle() 
             }
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func startOnce(_ action: @escaping () -> Void) -> some View {
+        if #available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *) {
+            self.task { action() }
+        } else {
+            self.onAppear { DispatchQueue.main.async { action() } }
+        }
     }
 }
