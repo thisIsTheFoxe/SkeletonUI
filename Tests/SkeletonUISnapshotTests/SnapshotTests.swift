e511eeaede2a6fbe334934bf763509624b162d41
Henry Storch <thisisthefoxe@gmail.com>
2026-05-23 19:39:48 +1200

    Remove all unused optional overloads and use explicit optional defaults instead
    - "don't fight the system/compiler/SDK"
    - also replace unused Identifiable conformance with Hashable

--- Tests/SkeletonUISnapshotTests/SnapshotTests.swift
+++ Tests/SkeletonUISnapshotTests/SnapshotTests.swift
@@ -3,40 +3,35 @@ import SnapshotTesting
 import SwiftUI
 import XCTest
 
-extension String: Identifiable {
-    public var id: UUID {
-        UUID()
-    }
-}
-
 final class SnapshotTests: XCTestCase {
     func testDefaultText() {
-        let one = Text(nil).skeleton(with: true)
-        let two = Text(verbatim: nil).skeleton(with: true)
+        let one = Text("").skeleton(with: true)
+        let two = Text("").skeleton(with: true)
         assertNamedSnapshot(matching: one, as: .image(size: CGSize(width: 100, height: 50)))
         assertNamedSnapshot(matching: two, as: .image(size: CGSize(width: 100, height: 50)))
     }
 
     func testCustomText() {
-        let view = Text(nil).skeleton(with: true, animation: .pulse(), appearance: .solid(), shape: .rectangle, lines: 2, scales: [1: 0.5])
+        let view = Text("").skeleton(with: true, animation: .pulse(), appearance: .solid(), shape: .rectangle, lines: 2, scales: [1: 0.5])
         assertNamedSnapshot(matching: view, as: .image(size: CGSize(width: 100, height: 50)))
     }
     
     func testCustomSize() {
         let size = CGSize(width: 100, height: 100)
-        let view = Text(nil).skeleton(with: true, size: size)
+        let view = Text("").skeleton(with: true, size: size)
         assertNamedSnapshot(matching: view, as: .image(size: size))
     }
 
     func testDefaultImage() {
         #if os(macOS)
-            let one = Image(nsImage: nil).skeleton(with: true)
+            let one = Image(nsImage: NSImage()).skeleton(with: true)
         #else
-            let one = Image(uiImage: nil).skeleton(with: true)
+            let one = Image(uiImage: UIImage()).skeleton(with: true)
         #endif
-        let two =  Image(nil, scale: .zero, label: Text(String())).skeleton(with: true)
-        let three = Image(nil).skeleton(with: true)
-        let four = Image(nil, label: Text(String())).skeleton(with: true)
+        let two = NSImage().cgImage(forProposedRect: nil, context: nil, hints: nil).map { Image($0, scale: .zero, label: Text("")) }
+            .skeleton(with: true)
+        let three = Image("").skeleton(with: true)
+        let four = Image("", label: Text(String())).skeleton(with: true)
         assertNamedSnapshot(matching: one, as: .image(size: CGSize(width: 100, height: 100)))
         assertNamedSnapshot(matching: two, as: .image(size: CGSize(width: 100, height: 100)))
         assertNamedSnapshot(matching: three, as: .image(size: CGSize(width: 100, height: 100)))
@@ -45,13 +40,14 @@ final class SnapshotTests: XCTestCase {
 
     func testCustomImage() {
         #if os(macOS)
-            let one = Image(nsImage: nil).skeleton(with: true, animation: .none, shape: .circle)
+            let one = Image("").skeleton(with: true, animation: .none, shape: .circle)
         #else
-        let one = Image(uiImage: nil).skeleton(with: true, animation: .none, shape: .circle)
+        let one = Image(optional: nil).skeleton(with: true, animation: .none, shape: .circle)
         #endif
-        let two =  Image(nil, scale: .zero, label: Text(String())).skeleton(with: true, animation: .none, shape: .circle)
-        let three = Image(nil).skeleton(with: true, animation: .none, shape: .circle)
-        let four = Image(nil, label: Text(String())).skeleton(with: true, animation: .none, shape: .circle)
+        let two = NSImage().cgImage(forProposedRect: nil, context: nil, hints: nil).map { Image($0, scale: .zero, label: Text("")) }
+            .skeleton(with: true, animation: .none, shape: .circle)
+        let three = Image("").skeleton(with: true, animation: .none, shape: .circle)
+        let four = Image("", label: Text(String())).skeleton(with: true, animation: .none, shape: .circle)
         assertNamedSnapshot(matching: one, as: .image(size: CGSize(width: 100, height: 100)))
         assertNamedSnapshot(matching: two, as: .image(size: CGSize(width: 100, height: 100)))
         assertNamedSnapshot(matching: three, as: .image(size: CGSize(width: 100, height: 100)))
@@ -60,7 +56,7 @@ final class SnapshotTests: XCTestCase {
 
     func testSystemImage() {
         #if os(macOS)
-            let one = Image(nil).skeleton(with: true)
+            let one = Image("").skeleton(with: true)
         let two = Image("checkmark").skeleton(with: true, animation: .none, shape: .circle)
         #else
             let one = Image(systemName: nil).skeleton(with: true)
@@ -71,67 +67,67 @@ final class SnapshotTests: XCTestCase {
     }
 
     func testDefaultTextField() {
-        let one = TextField(titleKey: nil, text: Binding.constant(String())).skeleton(with: true)
-        let two = TextField(titleKey: nil, value: Binding.constant(String()), formatter: NumberFormatter()).skeleton(with: true)
+        let one = TextField("", text: Binding.constant(String())).skeleton(with: true)
+        let two = TextField("", value: Binding.constant(String()), formatter: NumberFormatter()).skeleton(with: true)
         assertNamedSnapshot(matching: one, as: .image(size: CGSize(width: 100, height: 50)))
         assertNamedSnapshot(matching: two, as: .image(size: CGSize(width: 100, height: 50)))
     }
 
     func testCustomTextField() {
-        let one = TextField(titleKey: nil, text: Binding.constant(String())).skeleton(with: true, appearance: .gradient(.angular), shape: .ellipse)
-        let two = TextField(titleKey: nil, value: Binding.constant(String()), formatter: NumberFormatter()).skeleton(with: true, appearance: .gradient(.angular), shape: .ellipse)
+        let one = TextField("", text: Binding.constant(String())).skeleton(with: true, appearance: .gradient(.angular), shape: .ellipse)
+        let two = TextField("", value: Binding.constant(String()), formatter: NumberFormatter()).skeleton(with: true, appearance: .gradient(.angular), shape: .ellipse)
         assertNamedSnapshot(matching: one, as: .image(size: CGSize(width: 100, height: 50)))
         assertNamedSnapshot(matching: two, as: .image(size: CGSize(width: 100, height: 50)))
     }
 
     func testDefaultToggle() {
-        let one = Toggle(nil, isOn: Binding.constant(false)).skeleton(with: true)
-        let two = Toggle(isOn: nil, label: { Text(String()) }).skeleton(with: true)
+        let one = Toggle("", isOn: Binding.constant(false)).skeleton(with: true)
+        let two = Toggle(isOn: Binding.constant(Bool()), label: { Text(String()) }).skeleton(with: true)
         assertNamedSnapshot(matching: one, as: .image(size: CGSize(width: 100, height: 50)))
         assertNamedSnapshot(matching: two, as: .image(size: CGSize(width: 100, height: 50)))
     }
 
     func testCustomToggle() {
-        let one = Toggle(nil, isOn: Binding.constant(false)).skeleton(with: true, appearance: .gradient(.radial), shape: .rounded(.radius(10)))
-        let two = Toggle(isOn: nil, label: { Text(String()) }).skeleton(with: true, appearance: .gradient(.radial), shape: .rounded(.radius(10)))
+        let one = Toggle("", isOn: Binding.constant(false)).skeleton(with: true, appearance: .gradient(.radial), shape: .rounded(.radius(10)))
+        let two = Toggle(isOn: Binding.constant(Bool()), label: { Text(String()) }).skeleton(with: true, appearance: .gradient(.radial), shape: .rounded(.radius(10)))
         assertNamedSnapshot(matching: one, as: .image(size: CGSize(width: 100, height: 50)))
         assertNamedSnapshot(matching: two, as: .image(size: CGSize(width: 100, height: 50)))
     }
 
     func testDefaultSecureField() {
-        let view = SecureField(nil, text: Binding.constant(String())).skeleton(with: true)
+        let view = SecureField("", text: Binding.constant(String())).skeleton(with: true)
         assertNamedSnapshot(matching: view, as: .image(size: CGSize(width: 100, height: 50)))
     }
 
     func testCustomSecureField() {
-        let view = SecureField(nil, text: Binding.constant(String())).skeleton(with: true, appearance: .gradient(.angular), shape: .ellipse)
+        let view = SecureField("", text: Binding.constant(String())).skeleton(with: true, appearance: .gradient(.angular), shape: .ellipse)
         assertNamedSnapshot(matching: view, as: .image(size: CGSize(width: 100, height: 50)))
     }
 
     func testDefaultSkeletonList() {
         let view = SkeletonList(with: [#function]) { loading, item in
-            Text(item).skeleton(with: loading)
+            Text(item ?? "").skeleton(with: loading)
         }
         assertNamedSnapshot(matching: view, as: .image(size: CGSize(width: 100, height: 25)))
     }
 
     func testLoadingSkeletonList() {
         let view = SkeletonList(with: [String]()) { loading, item in
-            Text(item).skeleton(with: loading)
+            Text(item ?? "").skeleton(with: loading)
         }
         assertNamedSnapshot(matching: view, as: .image(size: CGSize(width: 100, height: 25)))
     }
 
     func testDefaultSkeletonForEach() {
         let view = SkeletonForEach(with: [#function]) { loading, item in
-            Text(item).skeleton(with: loading)
+            Text(item ?? "").skeleton(with: loading)
         }
         assertNamedSnapshot(matching: view, as: .image(size: CGSize(width: 100, height: 25)))
     }
 
     func testLoadingSkeletonForEach() {
         let view = SkeletonList(with: [String]()) { loading, item in
-            Text(item).skeleton(with: loading)
+            Text(item ?? "").skeleton(with: loading)
         }
         assertNamedSnapshot(matching: view, as: .image(size: CGSize(width: 100, height: 25)))
     }
